8f5bd3a21bbd36de37d3731eba92dcaad8231043
Nelly R. Palacios S <n.r.palacios.salinas@umail.leidenuniv.nl>
2020-11-25 19:28:28 +0100

    Deleted experiments/.gitkeep, experiments/Baseline.py files

--- experiments/Baseline.py
+++
@@ -1,8 +0,0 @@
-from datasets.datasets import create_dataset
-import autokeras as ak
-
-#run default ak classifier
-X1,X2,y1,y2=create_dataset('eurosat_rgb')
-clf = ak.ImageClassifier( overwrite=True,multi_label=True,max_trials=10)
-clf.fit(X1, y1)
-print(clf.evaluate(X2,y2))
